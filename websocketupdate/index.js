e664cccabfb87fdbe0730d02d733c7c66d0b758c
sharkawiii <y_elsh@live.concordia.ca>
2025-02-23 17:40:59 -0500

    Emitting events

--- websocketupdate/index.js
+++ websocketupdate/index.js
@@ -17,7 +17,11 @@ io.on('connection', (socket) => {
     console.log('user disconnected');
   });
 });
-
+io.on('connection', (socket) => {
+  socket.on('chat message', (msg) => {
+    console.log('message: ' + msg);
+  });
+});
 server.listen(3000, () => {
   console.log('server running at http://localhost:3000');
 });
