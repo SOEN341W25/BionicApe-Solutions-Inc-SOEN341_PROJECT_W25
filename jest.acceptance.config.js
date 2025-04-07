f5b813290ec909fa15323638f1f2d21110dde857
Marilakkuma <marikajita@gmail.com>
2025-04-07 16:42:11 -0400

    Delete jest.acceptance.config.js

--- jest.acceptance.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  preset: 'jest-puppeteer',
-  setupFilesAfterEnv: ['<rootDir>/tests/jest.setup.js'],
-  testMatch: ['**/tests/acceptance/**/*.test.js'],
-  testTimeout: 30000
-};
