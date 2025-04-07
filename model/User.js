588a80a317fcb4a013af0e48d103065b39f0622c
Marilakkuma <marikajita@gmail.com>
2025-04-07 16:44:59 -0400

    Create User.js

---
+++ model/User.js
@@ -0,0 +1,52 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    role: {
+        type: String,
+        enum: ['Admin', 'NormalUser'],
+        default: 'NormalUser'
+    },
+    channels: {
+        type: [String],
+        default: []
+    },
+    userDMs: [{
+        recipientUser: {
+            type: String,
+            required: true
+        },
+        messageIds: [{
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Message'
+        }]
+    }],
+    userStatus: {
+        type: String,
+        enum: ['online', 'offline', 'away'],
+        default: 'offline'
+    },
+    lastActivateAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+// Remove password when transforming to JSON
+userSchema.methods.toJSON = function() {
+    const obj = this.toObject();
+    delete obj.password;
+    delete obj._id;
+    return obj;
+};
+
+module.exports = mongoose.model('users', userSchema);
