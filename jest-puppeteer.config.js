7cb3fd2c02fed1a9130bbe3015cfb15f3b106504
Marilakkuma <marikajita@gmail.com>
2025-04-07 12:35:58 -0400

    Create jest-puppeteer.config.js

---
+++ jest-puppeteer.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  launch: {
+    headless: true,
+    args: ['--no-sandbox', '--disable-setuid-sandbox']
+  },
+  server: {
+    command: 'node app.js',
+    port: 3000,
+    launchTimeout: 10000,
+    debug: true
+  }
+};
