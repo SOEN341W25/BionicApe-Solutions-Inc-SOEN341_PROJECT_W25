641cdfb6a4492778603b7eccdec886e990151133
Marilakkuma <marikajita@gmail.com>
2025-04-10 13:34:56 -0400

    Update jest.unit.config.js

--- jest.unit.config.js
+++ jest.unit.config.js
@@ -3,7 +3,7 @@ module.exports = {
   testEnvironment: 'jsdom',
   setupFilesAfterEnv: [path.resolve(__dirname, './BACKEND_/tests/jest.setup.js')],
   moduleFileExtensions: ['js'],
-  testMatch: ['**/backend/tests/unit/**/*.test.js'],
+  testMatch: ['**/BACKEND_/tests/unit/**/*.test.js'],
   collectCoverage: true,
   coverageReporters: ['text', 'lcov', 'clover'],
   coverageDirectory: 'coverage-unit',
