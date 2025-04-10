9cc2a8a06729f3c6cd87a7b6bbf5c95ba4f72828
Marilakkuma <marikajita@gmail.com>
2025-04-10 12:34:37 -0400

    Update jest.integration.config.js

--- jest.integration.config.js
+++ jest.integration.config.js
@@ -1,6 +1,9 @@
+// jest.integration.config.js
+const path = require('path');
+
 module.exports = {
   testEnvironment: 'node',
-  setupFilesAfterEnv: ['<rootDir>/tests/jest.setup.js'],
+  setupFilesAfterEnv: [path.resolve('./tests/jest.setup.js')],
   moduleFileExtensions: ['js'],
   testMatch: ['**/tests/integration/**/*.test.js'],
   collectCoverage: true,
