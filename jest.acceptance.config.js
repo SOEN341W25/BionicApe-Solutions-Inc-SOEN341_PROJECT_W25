9cff2999f13c5f1edf94b97c053caad5c5c81893
Marilakkuma <marikajita@gmail.com>
2025-04-10 13:09:33 -0400

    Update jest.acceptance.config.js

--- jest.acceptance.config.js
+++ jest.acceptance.config.js
@@ -1,9 +1,7 @@
-// jest.acceptance.config.js
 const path = require('path');
-
 module.exports = {
   preset: 'jest-puppeteer',
-  setupFilesAfterEnv: [path.resolve('./tests/jest.setup.js')],
-  testMatch: ['**/tests/acceptance/**/*.test.js'],
+  setupFilesAfterEnv: [path.resolve(__dirname, './backend/tests/jest.setup.js')],
+  testMatch: ['**/backend/tests/acceptance/**/*.test.js'],
   testTimeout: 30000
 };
