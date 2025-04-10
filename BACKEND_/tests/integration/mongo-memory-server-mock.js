e96f992dfbffa1e4ffd3b3f6ad489b2b73955189
Marilakkuma <marikajita@gmail.com>
2025-04-10 14:44:16 -0400

    Create mongo-memory-server-mock.js

---
+++ BACKEND_/tests/integration/mongo-memory-server-mock.js
@@ -0,0 +1,31 @@
+// BACKEND_/tests/integration/mongo-memory-server-mock.js
+const MockMongoServer = {
+  instance: null,
+  
+  async start() {
+    console.log('Mock MongoDB server started');
+    this.instance = {
+      getUri: () => 'mongodb://localhost:27017/test-db',
+      stop: () => Promise.resolve()
+    };
+    return this.instance;
+  },
+  
+  getUri() {
+    return 'mongodb://localhost:27017/test-db';
+  },
+  
+  async stop() {
+    console.log('Mock MongoDB server stopped');
+    return Promise.resolve();
+  }
+};
+
+module.exports = {
+  MongoMemoryServer: {
+    create: async () => {
+      await MockMongoServer.start();
+      return MockMongoServer;
+    }
+  }
+};
