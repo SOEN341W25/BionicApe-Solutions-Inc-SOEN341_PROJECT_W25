2751d8d7d22c0c09dee878b69b241349e8ef4db6
Marilakkuma <marikajita@gmail.com>
2025-04-10 12:33:45 -0400

    Update jest.unit.config.js

--- jest.unit.config.js
+++ jest.unit.config.js
@@ -1,6 +1,7 @@
+const path = require('path');
 module.exports = {
   testEnvironment: 'jsdom',
-  setupFilesAfterEnv: ['<rootDir>/tests/jest.setup.js'],
+  setupFilesAfterEnv: [path.resolve(__dirname, './tests/jest.setup.js')],
   moduleFileExtensions: ['js'],
   testMatch: ['**/tests/unit/**/*.test.js'],
   collectCoverage: true,
