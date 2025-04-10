13f33106e1222ae107d22825cb0b36512cf1b7a1
Marilakkuma <marikajita@gmail.com>
2025-04-10 14:54:53 -0400

    Update jest.integration.config.js

--- jest.integration.config.js
+++ jest.integration.config.js
@@ -1,6 +1,7 @@
+// jest.integration.config.js
 const path = require('path');
 module.exports = {
-  testEnvironment: 'node',
+  testEnvironment: 'node', // Important: use node environment for mongoose
   setupFilesAfterEnv: [path.resolve(__dirname, './BACKEND_/tests/jest.setup.js')],
   moduleFileExtensions: ['js'],
   testMatch: ['**/BACKEND_/tests/integration/**/*.test.js'],
@@ -8,5 +9,9 @@ module.exports = {
   coverageReporters: ['text', 'lcov'],
   coverageDirectory: 'coverage-integration',
   verbose: true,
-  testTimeout: 30000
+  testTimeout: 30000,
+  // Add this to suppress mongoose warnings
+  globals: {
+    SUPPRESS_JEST_WARNINGS: true
+  }
 };
